eb73967b1a17668ef6b00e7e9a7f8b1633c49675
Samyak <samyak.coll1905@gmail.com>
2026-04-27 03:02:32 +0530

    resolved cors

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 //library imports
-require("dotenv").config({path:"./.env"});
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 //controller imports
-const {createStudent, getStudent} = require("./controller/studentController")
-const {createTeacher, getTeacher} = require("./controller/teacherController")
+const { createStudent, getStudent } = require("./controller/studentController")
+const { createTeacher, getTeacher } = require("./controller/teacherController")
 
 //router imports
-const {studentRouter} = require("./router/studentRouter");
-const {teacherRouter} = require("./router/teacherRouter")
+const { studentRouter } = require("./router/studentRouter");
+const { teacherRouter } = require("./router/teacherRouter")
 
 //middleware imports
-const {verifyTokenLogin} = require("./middleware/verifyToken");
+const { verifyTokenLogin } = require("./middleware/verifyToken");
 
 //env imports
 const PORT = process.env.PORT;
@@ -24,36 +24,37 @@ const app = express();
 
 //middleware
 app.use(cors({
-  exposedHeaders: ['x-auth-token'], 
+    origin: "*",
+    exposedHeaders: ['x-auth-token'],
 }));
-app.use(express.json({limit : '50mb'}));
-app.use(express.urlencoded({extended : true, limit : '50mb'}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 //routings
 app.use("/student", studentRouter);
 app.use("/teacher", teacherRouter);
 
-app.get("/test", (req,res)=>{res.send("server running")});
+app.get("/test", (req, res) => { res.send("server running") });
 app.post("/verify", verifyTokenLogin);
 
 //todo
 app.post("/teacherLogin", getTeacher)
 app.post("/studentLogin", getStudent)
 
-app.get("/testupdate",(req,res)=>{res.send("updated code 2")})
+app.get("/testupdate", (req, res) => { res.send("updated code 2") })
 
 
 app.post("/createTeacher", createTeacher)
 
-const startServer = async ()=>{
-    try{
+const startServer = async () => {
+    try {
         await mongoose.connect(`${MONGODBURL}`);
         console.log("Database connected successfully");
 
-        app.listen(PORT, ()=>{
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`server started on http://localhost:${PORT}`);
         })
-    }catch(err){
+    } catch (err) {
         console.error("failed to start server\n", err);
     }
 }
